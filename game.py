de6e5258fc89efc4987ac3d77c37d56f33c90bd1
Jordonez123 <jio2108@columbia.edu>
2025-06-25 13:40:12 -0400

    1. Added test_set_player_order logic. 2. Added set_player_order_logic. 3. Began scaffolding logic for the play_game funct function.

--- game.py
+++ game.py
@@ -1,20 +1,32 @@
-from player_abc import Player
+from player import PlayerImpl
 from board import Board
 
 class Game:
-    def __init__(self, player1: Player, player2: Player, board: Board):
+    def __init__(self, player1: PlayerImpl, player2: PlayerImpl, board: Board):
         self.player1 = player1
-        self.player = player2
+        self.player2 = player2
         self.board = board
         self.game_winner = None
     
-    def set_player_order(self):
-        pass
+    def set_player_order(self, player1_order: int, player2_order: int):
+        self.player1.player_id = player1_order
+        self.player2.player_id = player2_order
     
     def play_game(self):
-        pass
+        # ask players for their names and assign playing order
+        self.set_up_players()
+        # main game loop
+        while True:
+            # ask player 1 for their move
+            player1_move = input("Player 1, what is your move?: ")
+            # add player1's move to the board
+            # check for a winner
+                # if yes, update self.game_winner and break the loop
+            # ask player 2 for their move
+            # check for a winner
+                # if yes, update self.game_winner and break the loop
 
-    def get_game_winner(self) -> Player:
+    def get_game_winner(self) -> PlayerImpl:
         return self.game_winner
 
     def _get_game_captions(self):
--- tests/test_game.py
+++ tests/test_game.py
@@ -1,1 +1,28 @@
 import unittest
+from board import Board
+from game import Game
+from player import PlayerImpl
+
+class TestGame(unittest.TestCase):
+    # -------------------------- Set Up and Tear Down ------------------------------------
+    def setUp(self):
+        self.player1 = PlayerImpl("Jordan")
+        self.player2 = PlayerImpl("Jennifer")
+        self.board = Board()
+        self.game = Game(self.player1, self.player2, self.board)
+
+    def tearDown(self):
+        self.player1 = None
+        self.player2 = None
+        self.board = None
+        self.game = None
+
+    def test_set_player_order(self):
+        # player 1 goes first
+        # player 2 goes second
+        self.game.set_player_order(0, 1)
+        self.assertEqual(self.player1.player_id, 0)
+        self.assertEqual(self.player2.player_id, 1)
+
+if __name__ == "__main__":
+    unittest.main()
